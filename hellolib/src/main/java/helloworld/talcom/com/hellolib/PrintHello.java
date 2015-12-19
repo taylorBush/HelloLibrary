4735a56647d26996753a41b38ea5b058ff18f70d
liangxianhong <liangxh2008@163.com>
2015-12-19 23:41:56 +0800

    first commit

---
+++ hellolib/src/main/java/helloworld/talcom/com/hellolib/PrintHello.java
@@ -0,0 +1,12 @@
+package helloworld.talcom.com.hellolib;
+
+import android.util.Log;
+
+/**
+ * Created by liangxianhong on 15/12/19.
+ */
+public class PrintHello {
+    public void showHello() {
+        Log.d("PrintHello", "hello lib");
+    }
+}
